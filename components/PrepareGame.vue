f84be0672eda31664c55613d5d9c58379e1606d6
fantashi099 <18521492@gmail.com>
2021-07-14 02:29:47 +0700

    Update UI in the main page, required dat.gui to create threejs's tabel setting.

--- components/PrepareGame.vue
+++ components/PrepareGame.vue
@@ -1,11 +1,19 @@
 <template>
   <div class="sidebar">
+    <h3 class="header">Final Project</h3>
+    <hr>
     <div>
-      <div>Select model</div>
+      <div class="choose-title">Select model</div>
       <select v-on:change="onModelChange($event)" class="form-control">
         <template v-for="model in models">
           <option :value="model.path">{{ model.name }}</option>
         </template>
+      </select>
+      <div class="choose-title">Background Color</div>
+      <input type="color" value="#ffffff" class="form-control" />
+      <div class="choose-title">Music</div>
+      <select v-on:change="onModelChange($event)" class="form-control">
+        <option value=0 >None</option>
       </select>
     </div>
   </div>
@@ -28,7 +36,27 @@ export default {
 </script>
 
 <style scoped>
-.sidebar {
-  background-color: #f1f1f1;
-}
+  hr {
+    margin-top: 0; 
+    width: 80%;
+    border-style: none;
+    border-top-style: dotted;
+    border-color: grey;
+  }
+
+  .header {
+    padding: 1rem 0rem; 
+    color: #125D98;
+    text-align: center;
+  }
+
+  .choose-title {
+    color: #3C8DAD;
+    text-align: center;
+    padding: 0.5rem 1rem;
+  }
+
+  .sidebar {
+    background-color: #f1f1f1;
+  }
 </style>
